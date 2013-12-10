6015f15c8bbd4ef458f860f61aece0d76720b000
Noah Klugman <nklugman@umich.edu>
2013-12-09 22:36:07 -0500

    added in the control skeleton

---
+++ android/core/src/umich/hijack/core/Control.java
@@ -0,0 +1,52 @@
+package umich.hijack.core;
+
+
+import umich.hijack.core.Packet;
+import umich.hijack.core.PacketDispatch;
+import umich.hijack.core.PktRecvCb;
+import umich.hijack.core.SerialDecoder;
+
+import android.annotation.SuppressLint;
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
+import android.os.Environment;
+
+public class Control {
+	
+	private SerialDecoder _serialDecoder;
+	private PacketDispatch _dispatcher;
+	
+	private void pinMode() {
+
+	}
+	
+	private void digitalWrite() {
+		
+	}
+	
+	private void digitalRead() {
+		
+	}
+	
+	private void analogRead() {
+		
+	}
+	
+	private void analogWrite() {
+		
+	}
+	
+	private void interrupts() {
+		
+	}
+	
+	private void noInterrupts() {
+		
+	}
+	
+	
+
+}
